ea63d235f7c138fcfbed626e4d8c69b38e844f12
Jerson Satoru Uyekita <jersonsatoru@yahoo.com.br>
2021-07-16 01:38:36 -0300

    feat: add webpack

---
+++ src/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Header() {
+    return <h1>Cabeçalho</h1>
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
+import { render } from 'react-dom'
+import Header from './Header'
 
-function App() {
-    return <h1>Hello world</h1>
-}
+render(<Header />, document.getElementById('root'))
